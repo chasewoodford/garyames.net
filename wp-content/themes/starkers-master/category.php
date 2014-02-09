ba2bb732bd671c0faaa3764a3b84f2ab1d7b9c73
cwoodford <chase@chasewoodford.com>
2014-02-09 11:06:32 -0500

    updated loop for proper display of category page elements

--- wp-content/themes/starkers-master/category.php
+++ wp-content/themes/starkers-master/category.php
@@ -18,12 +18,22 @@
 
         <?php global $query_string; query_posts($query_string . "&order=ASC"); ?>
         <?php if ( have_posts() ): ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-        <?php $chapterNumber = get_post_meta($post->ID, 'chapter_number', true); ?>
+
+        <!-- Add counter to posts -->
+        <?php $count = 0; while ( have_posts() ) : the_post(); $count++; ?>
+
+        <!-- If first post, put chapter number, category and description above post -->
+        <?php if ($count == 1) :
+            $chapterNumber = get_post_meta($post->ID, 'chapter_number', true);
+            $categories = get_the_category(); $category_id = $categories[0]->cat_ID;
+        ?>
 
         <header>
             <h1><?php echo $chapterNumber; ?> <span><?php echo single_cat_title( '', false ); ?></span></h1>
         </header>
+
+        <?php echo category_description( $category_id ); ?>
+
         <ol class="slats">
             <li>
                 <article>
@@ -31,8 +41,23 @@
                     <?php the_excerpt(); ?>
                 </article>
             </li>
-            <?php endwhile; ?>
+
+        <!-- For each post after the first, only show the title and excerpt -->
+        <?php else : ?>
+
+            <li>
+                <article>
+                    <h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                    <?php the_excerpt(); ?>
+                </article>
+            </li>
+        <?php endif; ?>
+
+        <?php endwhile; ?>
+
         </ol>
+
+        <!-- If there are no posts in the category for some reason... -->
         <?php else: ?>
         <h2>No posts to display in <?php echo single_cat_title( '', false ); ?></h2>
         <?php endif; ?>
